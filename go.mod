291f7dee0477c0a865457a1295548266c3ee8456
Xuan Huy <xuanhuy900@gmail.com>
2020-10-16 22:29:14 +0700

    Connect database

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module backend-github-trending
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.1.17
+require (
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.8.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"backend-github-trending/db"
 	"backend-github-trending/handler"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	sql:= &db.Sql{
+		Host: "localhost",
+		Port: 5432,
+		UserName: "Huy",
+		Password: "huydoi1955",
+		DbName: "Golang",
+	}
+	sql.Connect()
+
+	defer sql.Close()
+
 	e := echo.New()
 	e.GET("/", handler.Welcome)
 	e.GET("/user/sign-in", handler.HandleSignIn)
